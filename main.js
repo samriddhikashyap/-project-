28c7dc80cca08b798b919db5036c9bb47d986089
samriddhikashyap <109653749+samriddhikashyap@users.noreply.github.com>
2022-11-28 18:38:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+leftwristX = 0;
+rightwristX = 0;
+function setup() 
+{
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+ 
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+ 
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+ 
+function modelLoaded()
+{
+  console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " +noseX+ "noseY = " +noseY);
+    leftwristX= results[0].pose.leftWrist.x;
+    rightwristX= results[0].pose.rightWrist.x;
+    difference = floor(leftwristX - rightwristX);
+    console.log("leftwristX = "+leftwristX+"rightwristX =  "+rightwristX + "difference =" + difference);
+  }
+}
+
+
+
+
+function draw() {
+background('#6C91C2');
+
+  document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px";
+textSize(difference);
+fill('#FFE787');
+text('Peter', 50, 400);
+}
